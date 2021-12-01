c7199f72d88320a43b35022ee8b839ff6a346c8e
IMPWONG <matisse.acheen.pro@gmail.com>
2021-12-01 16:23:39 -0500

    change to ipv6

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
-export const nftaddress = "0xf39fd6e51aad88f6f4ce6ab8827279cfffb92266";
-export const nftmarketaddress = "0x0165878A594ca255338adfa4d48449f69242Eb8F";
+export const nftaddress = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512";
+export const nftmarketaddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
