c6c7c8cd709a8c877a54ed200ea8b89429bf5ad7
IMPWONG <matisse.acheen.pro@gmail.com>
2021-12-06 14:45:56 -0500

    loading state correction loader -> loaded

--- pages/create-item.js
+++ pages/create-item.js
@@ -1,5 +1,5 @@
-import { useState } from "react";
 import { ethers } from "ethers";
+import { useState } from "react";
 import { create as ipfsHttpClient } from 'ipfs-http-client';
 import { useRouter } from 'next/router';
 import Web3Modal from "web3modal";
