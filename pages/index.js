6aaffabdced4368151e17f6f7ded031b4c901e87
IMPWONG <matisse.acheen.pro@gmail.com>
2021-12-12 16:59:12 -0500

    reinstal node_modules

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,6 @@ export default function Home() {
     return (
       <Layout>
         <h1 className="flex justify-center text-blue-700 lg:text-3xl border-blue-600 border-b-2	text-bold">Welcome to the IMP NFT MARKETPLACE</h1>
-        <Chains />
       </Layout>
     ); 
   }
@@ -24,3 +23,4 @@ export default function Home() {
 
     );
 }  
+uer 
