853e8f25ac272eeff664038fcb4c4ef193128f22
IMPWONG <matisse.acheen.pro@gmail.com>
2021-12-12 23:47:43 -0500

    styles and network selector

--- src/components/Chains/Chains.js
+++ src/components/Chains/Chains.js
@@ -1,8 +1,11 @@
 import { Fragment, useEffect, useState } from "react";
 import { useMoralis } from "react-moralis";
 import { useChain } from "react-moralis";
+
 import { CheckIcon, SelectorIcon } from '@heroicons/react/solid';
+
 import { Listbox, Transition } from '@headlessui/react'
+
 import { AvaxLogo, PolygonLogo, BSCLogo, ETHLogo } from "./Logos";
 
 const networks = [
@@ -81,64 +84,72 @@ export default function Chains() {
   };
 
   return (
-    
-    <div className="w-72 fixed top-16">
-      <Listbox value={selectedNetwork} onChange={setSelectedNetwork}>
-        <div className="relativ mt-1">
-          <Listbox.Button className="relative w-full py-2 pl-3 pr-10 text-left bg-white rounded-lg shadow-md cursor-default focus:outline-none focus-visible:ring-2 focus-visible:ring-opacity-75 focus-visible:ring-white focus-visible:ring-offset-orange-300 focus-visible:ring-offset-2 focus-visible:border-indigo-500 sm:text-sm">
-            <span icon={selectedNetwork?.icon} className="block truncate">{selectedNetwork.value}</span>
-            <span className="absolute inset-y-0 right-0 flex items-center pr-2 pointer-events-none">
-              <SelectorIcon
-                className="w-5 h-5 text-gray-400"
-                aria-hidden="true"
-              />
+      <div className="ml-10">
+        <Listbox as="div" value={selectedNetwork} onChange={setSelectedNetwork}>
+         {({ open }) => (
+          <>
+            <div className="relativ mt-1">
+            <span className="inline-block w-full">
+              <Listbox.Button className="relative w-full py-2 pl-3 pr-10 text-left bg-gray-800 text-gray-300 hover:bg-gray-700 hover:text-white rounded-lg shadow-md cursor-default focus:outline-none focus-visible:ring-2 focus-visible:ring-opacity-75 focus-visible:ring-white focus-visible:ring-offset-orange-300 focus-visible:ring-offset-2 focus-visible:border-indigo-500 sm:text-sm">
+                <span className="sr-only">Open network menu</span>
+                <span className="block truncate">{selectedNetwork.value}</span>
+                <span className="absolute inset-y-0 right-0 flex items-center pr-2 pointer-events-none">
+                  <SelectorIcon
+                    className="w-5 h-5 text-gray-400"
+                    aria-hidden="true"
+                  />
+                </span>
+              </Listbox.Button>
             </span>
-          </Listbox.Button>
-          <Transition
-            as={Fragment}
-            leave="transition ease-in duration-100"
-            leaveFrom="opacity-100"
-            leaveTo="opacity-0"
-          >
-          <Listbox.Options className="absolute w-full py-1 mt-1 overflow-auto text-base bg-white rounded-md shadow-lg max-h-60 ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
-            {networks.map((item) => (
-              <Listbox.Option
-                key={item.key}
-                value={item.value}
-                icon={item.icon}
-                className={({ active }) =>
-                    `${active ? 'text-amber-900 bg-amber-100' : 'text-gray-900'}
-                          cursor-default select-none relative py-2 pl-10 pr-4`
-                }
-                >
-              {({ selectedNetwork, active }) => (
-                    <>
+            <Transition
+              show={open}
+              leave="transition ease-in duration-100"
+              leaveFrom="opacity-100"
+              leaveTo="opacity-0"
+            >
+            <Listbox.Options static className="absolute w-full py-1 mt-1 overflow-auto text-base bg-white rounded-md shadow-lg max-h-60 ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
+              {networks.map((item) => (
+                <Listbox.Option
+                  key={item.key}
+                  value={item.value}
+                  icon={item.icon}>
+                  {({ selected, active }) => (
+                    <div 
+                      className={`${
+                        active 
+                          ? "text-white bg-indigo-600"
+                          : "text-gray-900"
+                      } cursor-default select-none relative py-2 pl-10 pr-4`}
+                    >
                       <span
                         className={`${
-                          selectedNetwork ? 'font-medium' : 'font-normal'
-                        } block truncate`}
+                          selected ? "font-medium" : "font-normal"
+                        }`}
                       >
                         {item.value}
                       </span>
-                      {selectedNetwork ? (
+                      {selected && (
                         <span
                           className={`${
-                            active ? 'text-amber-600' : 'text-amber-600'
+                            active ? "text-white" : "text-indigo-600"
                           }
-                                absolute inset-y-0 left-0 flex items-center pl-3`}
+                            absolute inset-y-0 left-0 flex items-center pl-3`}
                         >
-                          <CheckIcon className="w-5 h-5" aria-hidden="true" />
-                        </span>
-                      ) : null}
-                    </>
-                  )}
-                </Listbox.Option>
-              ))}
-            </Listbox.Options>
-          </Transition>
-        </div>
-      </Listbox>
-    </div>    
+                        <CheckIcon className="w-5 h-5" aria-hidden="true" />
+                       </span>
+                       )}
+                    </div>
+                              )}
+                            </Listbox.Option>
+                          ))}
+                        </Listbox.Options>
+                      </Transition>
+                    </div>
+          </>
+  )}
+        </Listbox>
+               
+      </div>   
   );
 }
 
