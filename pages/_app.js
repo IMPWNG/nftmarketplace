a5370752fe4e379dd0f0ad5b2405b216dc9a88fc
IMPWONG <matisse.acheen.pro@gmail.com>
2021-12-13 16:05:32 -0500

    Auth

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,14 @@
 import React from 'react'
 import { MoralisProvider } from "react-moralis";
+import { ChakraProvider, extendTheme } from '@chakra-ui/react';
 
 import '../styles/globals.css';
+
+const theme = extendTheme({
+  config: {
+    initialColorMode: 'dark',
+  },
+});
 
 // Change to use .env
 const appId = "L8kuo2v3DlsVmjGfmKNYWZq3W0ZQQSdADNav4hum";
@@ -9,12 +16,12 @@ const serverUrl = "https://zz6umijjwcmt.usemoralis.com:2053/server";
 
 function MyApp({ Component, pageProps }) {
     return (
-      <MoralisProvider 
-        appId={appId} 
-        serverUrl={serverUrl}
-      >
-        <Component {...pageProps} />
+      <MoralisProvider appId={appId} serverUrl={serverUrl}>
+        <ChakraProvider theme={theme}>
+          <Component {...pageProps} />
+        </ChakraProvider>
       </MoralisProvider>
     );
   }
 export default MyApp
+ 
--- pages/index.js
+++ pages/index.js
@@ -1,32 +1,32 @@
-import React from "react";
-
+import { useState } from "react";
 import { useMoralis } from "react-moralis";
-import { Button } from '@chakra-ui/button';
-
-import Head from "next/head";
+import { Button } from '@chakra-ui/react';
+import { Container, Heading } from '@chakra-ui/layout';
 
 import Layout from "../src/components/Layout";
 import Chains from "../src/components/Chains/Chains";
-
+import { Auth } from "./Auth";
+  
 export default function Home() {
-  const { authenticate, isAuthenticated, isAuthentificating, logout } = useMoralis();
+  const { isAuthenticated } = useMoralis();
 
   if (isAuthenticated) {
-  
     return (
-      <div>
-        <Head>
-          <title>NFT MARKETPLACE</title>
-        </Head>
+      <Container>
+        <Heading className="flex justify-center text-blue-700 lg:text-3xl border-blue-600 border-b-2	text-bold">
+          Welcome to your NFT Marketplace
+        </Heading>
         <Layout />
-          <h1 className="flex justify-center text-blue-700 lg:text-3xl border-blue-600 border-b-2	text-bold">Welcome to the IMP NFT MARKETPLACE</h1>
-      </div>
-    ); 
-  }
-    return (
-      <div>
-        <Button isLoading={isAuthentificating} onClick={() => authenticate()} className="bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded">Web 3 - Login</Button>
-      </div>
+      </Container>
 
+    ); 
+  };
+    return (
+      <Container className="items-center">
+        <Heading mb={6} className="flex justify-center text-blue-700 lg:text-3xl border-blue-600 border-b-2	text-bold">
+          Welcome to your NFT Marketplace
+        </Heading>
+        <Auth />
+      </Container>
     );
 }  
