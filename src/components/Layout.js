7e856c0b611d032c1e3f4fc5d3ace758662dd23d
IMPWONG <matisse.acheen.pro@gmail.com>
2021-12-12 21:19:42 -0500

    clean up

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,17 +1,11 @@
-import Head from "next/head";
 import Navbar from "./Navbar";
 
-const Layout = (props) => {
-  const { title, metaKeywords, metaDesc } = props.metaInfo;
+export default function Layout() {
   return (
     <>
-      <Head>
-        <title>NFT MARKETPLACE</title>
-      </Head>
         <div className="flex flex-col min-h-screen bg-gray-100">
           <Navbar />
             <div id="content" className="flex-grow mx-auto p-8 w-full shadow rounded bg-white">
-              {props.children}
             </div>
             <footer className="w-full text-center border-t bg-blue-700 text-white p-4 pin-b">
               <span className="lg:text-xl"> &copy; IMPWNG 2021 </span>
@@ -20,12 +14,4 @@ const Layout = (props) => {
     </>
   );
 }
-Layout.defaultProps = {
-  metaInfo: {
-    title: "Default Title",
-    metaKeywords: "Default metaKeywords",
-    metaDesc: "Default metaDesc",
-  },
-};
 
-export default Layout;
